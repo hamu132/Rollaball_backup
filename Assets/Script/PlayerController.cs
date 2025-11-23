6de633dfd03284be3be65be96ca356c56b1a041d
hamu132 <guba06120@gmail.com>
2025-11-23 14:14:29 +0900

    チュートリアルに合わせた

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -40,7 +40,7 @@ public class PlayerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if (count >= 3)
+        if (count >= 12)
         {
             winTextObject.SetActive(true);
         }
